d1e8b665f20fbf807b9f2764c28c81905c374a74
Halim <ahmed.halim162097@gmail.com>
2022-06-28 14:29:39 +0200

    changing data fetched from the api

--- Server/routes/api.js
+++ Server/routes/api.js
@@ -4,18 +4,33 @@ var router = express.Router();
 router.get("/todo", function (req, res, next) {
   data = [
     {
-      id: 1,
-      title: "Grab some Pizza",
-      completed: true,
+      id: "671876e7-0714-4217-a7ef-2d15f8be1aa9",
+      title: "JS ES6 📚",
+      completed: false,
     },
     {
-      id: 2,
-      title: "Do your workout",
-      completed: true,
+      id: "448f412d-243b-464d-94da-d0134c958421",
+      title: "React.JS Course ⚛️",
+      completed: false,
     },
     {
-      id: 3,
-      title: "Hangout with friends",
+      id: "c339ffc0-7bba-4297-b886-b38b49892230",
+      title: "Redux Course 🔮",
+      completed: false,
+    },
+    {
+      id: "60239fbe-225b-4579-ab93-80a3ca1b8d00",
+      title: "Git Version Control 🗄",
+      completed: false,
+    },
+    {
+      id: "728b9b73-aaab-44bb-ace6-398eed99d3c8",
+      title: "Interview Questions 👨🏻‍💻 ",
+      completed: false,
+    },
+    {
+      id: "eae5fe2f-ab3f-4997-919d-90f2f1ff1992",
+      title: "Apply 🎯",
       completed: false,
     },
   ];
